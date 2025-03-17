c3fc0f1ab5d9e951f773a64ee3b4c1c48254b719
Hassan Sheraz <hsnshrz@gmail.com>
2025-03-17 14:31:26 -0500

    Updated database schema and relationships

--- mysql-init/03_create_doctors_table.sql
+++
@@ -1,8 +0,0 @@
-USE testdb;
-
-CREATE TABLE IF NOT EXISTS doctors (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    specialty VARCHAR(255) NOT NULL
-);
